fd3f313eb04802ffcfd627b5e8b7b96019ede878
mcube3567 <mcube3567@gmail.com>
2019-12-12 22:43:12 +0100

    scriting

--- file.js
+++ file.js
@@ -65,3 +65,61 @@ true != true // false
 // Make the string: "HI There! It's sunny out" by using the + sign.
 "Hi There! " + "It\'s \"sunny\" out"
 "hi there ! " + "it\'s \"sunny\" out"
+var geoge = "my sirt is thorn";
+var first = prompt('what is your user name');
+var second = prompt('what is your first name');
+var sum = Number(first) +  Number(second);
+alert('the sum is '+ sum);
+var a = true;
+a = "hello";
+var b; 
+
+// exercise on variables.
+
+// Evaluate what answers you would get if you ran this in the
+// Javascript Console in Google Chrome. Answer the questions then 
+// check them by copying them and running it in the console yourself 
+// line by line 
+
+
+// add variable "firstName" and "lastName" // so that they equal your name
+
+// create a variable that holds the answer // of "firstName" + " " + "lastName"
+
+// Evaluate this question. What is a + b?
+var a = 34;
+var b = 21;
+a = 2;
+a + b // what is the answer here?
+
+// What is c equal to?
+var c; undefined;
+var firstName = 'moh';
+var secondName = "muhk";
+var c = firstName + "" + secondName;
+//solution;
+// Evaluate what answers you would get if you ran this in the
+// Javascript Console in Google Chrome. Answer the questions then 
+// check them by copying them and running it in the console yourself 
+// line by line 
+
+
+// add variable "firstName" and "lastName" // so that they equal your name
+var firstName = "Andrei";
+var lastName = "Neagoie";
+
+// create a variable that holds the answer // of "firstName" + " " + "LastName"
+var fullName = firstName + " " + lastName;
+
+// Evaluate this question. What is a + b?
+var a = 34;
+var b = 21;
+a = 2;
+b + a // 23
+
+// What is c equal to?
+var c; // undefined
+
+
+
+
