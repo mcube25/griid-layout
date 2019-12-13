208c83ab21a50d3ebcca0a3d50d3c04a17c19f2c
mcube3567 <mcube3567@gmail.com>
2019-12-13 21:48:14 +0100

    script

--- file.js
+++ file.js
@@ -119,7 +119,71 @@ b + a // 23
 
 // What is c equal to?
 var c; // undefined
+//exercise 3.
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.  
 
 
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+var first = prompt('what is your user name');
+var second = prompt('what is your first name');
+var sum = Number(first) +  Number(second);
+alert('the sum is '+ sum);
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+var firstNumber = prompt("Please provide the first number"); 
+var secondNumber = prompt("Please provide the second number"); 
+alert(Number(firstNumber) + Number(secondNumber));
+//awesome.
+//javascript conditionals;
+var name = 'billy'
+if (name === 'billy'){
+    alert('hi billy');
+} else {
+    alert('hmm i dont know u');
+} else if (name==='susi') {
+    alert('hi sussi');
+}
+//logical operations || && !. 
+if (name === 'billy' || name ==='sussi'){
+    alert ('hi billy or sussi');
+}
+// Make a keyless car!
+// This car will only let you drive if you are over 18. Make it do the following:
 
 
+//using prompt() and alert(), ask a user for their age.
+// IF they say they are below 18, respond with:
+// "Sorry, you are too young to drive this car. Powering off"
+
+// IF they say they are 18, respond with:
+// "Congratulations on your first year of driving. Enjoy the ride!"
+
+// IF they say they are over 18, respond with:
+// "Powering On. Enjoy the ride!"
+// Make a keyless car!
+// This car will only let you drive if you are over 18. Make it do the following:
+
+
+// using prompt() and alert(), ask a user for their age.
+// IF they say they are below 18, respond with:
+// "Sorry, you are too young to drive this car. Powering off"
+
+// IF they say they are 18, respond with:
+// "Congratulations on your first year of driving. Enjoy the ride!"
+
+// IF they say they are over 18, respond with:
+// "Powering On. Enjoy the ride!"
+
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+	alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+	alert("Powering On. Enjoy the ride!");
+} else if (Number(age) === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
