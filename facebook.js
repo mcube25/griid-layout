529f78c3be7569b08702d1321838dff23e33d0c9
mcube3567 <mcube3567@gmail.com>
2019-12-15 21:13:54 +0100

    book

---
+++ facebook.js
@@ -0,0 +1,27 @@
+var database = [
+    {
+        username:'pale',
+        passwor: 'supercorrect'
+    }
+];
+var newsfeed = [
+    {
+        username:'bobby',
+        timeline:'so tired from all these js',
+},
+{
+    username:'sally',
+    timeline:'js is so cool',  
+}
+];
+var usernamePrompt= prompt(('whats your username');
+var passwordPrompt= prompt(('whats your passwor');
+function signIn(user, pass){
+    if (user ===database[0].username && pass===database[0].password){
+        console.log(newsfeed);
+    } else {
+        alert('sorry wrong user or password');
+    }
+}
+//signIn(usernameprompt, passwordprompt);
+signIn(usernamePrompt, passwordPrompt);
--- script.js
+++ script.js
@@ -46,7 +46,78 @@ function checkDriverAge() {
 
 //BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
 checkDriverAge(92);
-it returns "Powering On. Enjoy the ride!"
+//it returns "Powering On. Enjoy the ride!"
 //data structure.arrays
-var list = ['bear''lion''dog''bird'];
+var list = ['bear','lion','bird'];
 console.log(list[1]);
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+ var objects ={
+    username:'muhammad',
+    password:'unknown',
+}
+var database =[ {
+    username:'muhammad',
+    password:'unknown',
+}]
+var newsfeed =[ 
+    {
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
+//array exercise.
+// using this array,
+ var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+// 1. Remove the Banana from the array.
+
+// 2. Sort the array in order.
+
+// 3. Put "Kiwi" at the end of the array.
+
+// 4. Remove "Apples" from the array.
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+
+you should have at the end:
+["Kiwi", "Oranges", "Blueberries"]
+
+// using this array,
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+// 1. Remove the Banana from the array.
+array.shift();
+
+// 2. Sort the array in order. 
+array.sort();
+
+// 3. Put "Kiwi" at the end of the array.
+array.push("Kiwi");
+
+// 4. Remove "Apples" from the array.
+array.splice(0, 1);
+
+// 5. Sort the array in reverse order. 
+array.reverse();
+
+// using this array, 
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+array2[1][1][0];
