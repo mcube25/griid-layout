ef8d6167b00d95600b75237ade83c58f86def49e
mcube3567 <mcube3567@gmail.com>
2019-12-17 23:29:32 +0100

    domselectors

--- facebook.js
+++ facebook.js
@@ -25,3 +25,32 @@ function signIn(user, pass){
 }
 //signIn(usernameprompt, passwordprompt);
 signIn(usernamePrompt, passwordPrompt);
+DOM Selectors
+--------------
+getElementsByTagName
+getElementsByClassName
+getElementById
+
+querySelector
+querySelectorAll
+
+getAttribute
+setAttribute
+
+##Changing Styles
+style.{property} //ok
+
+className //best
+classList //best
+
+classList.add
+classList.remove
+classList.toggle
+
+##Bonus
+innerHTML //DANGEROUS
+
+parentElement
+children
+
+##It is important to CACHE selectors in variables
